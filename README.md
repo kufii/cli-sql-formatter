32e73885cbe46c503f464bc8400746775749558d
Adrien Pyke <adpyke@gmail.com>
2018-09-29 00:23:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# cli-sql-formatter
+
+A console interface for [sql-formatter](https://github.com/zeroturnaround/sql-formatter)
+
+## Installation
+
+This tool can be installed via npm
+
+```
+npm install -g cli-sql-formatter
+```
+
+## Usage
+
+```
+Usage: cli-sql-formatter [options]
+
+Options:
+
+  -V, --version            output the version number
+  -f, --file <file>        load SQL from a file
+  -o, --out <file>         output results to a file
+  -d, --dialect <dialect>  which dialect to format with (sql: Standard SQL, n1ql: Couchbase N1QL, db2: IBM DB2, pl/sql: Oracle PL/SQL) (default: sql)
+  -i, --indent <n>         number of spaces to indent with (default: 2)
+  -t, --tabs               indent with tabs
+  -h, --help               output usage information
+
+$ echo SELECT * FROM table WHERE n = 1 | node cli-sql-formatter.js
+SELECT
+  *
+FROM
+  table
+WHERE
+  n = 1
+```
---
+++ cli-sql-formatter.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { version } = require('./package.json');
+const { promisify } = require('util');
+const fs = require('fs');
+const program = require('commander');
+const getStdin = require('get-stdin');
+const sqlFormatter = require('sql-formatter');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+program
+	.version(version)
+	.usage('[options]')
+	.option('-f, --file <file>', 'load SQL from a file')
+	.option('-o, --out <file>', 'output results to a file')
+	.option('-d, --dialect <dialect>', 'which dialect to format with (sql: Standard SQL, n1ql: Couchbase N1QL, db2: IBM DB2, pl/sql: Oracle PL/SQL)', /^(sql|n1ql|db2|pl\/sql)$/i, 'sql')
+	.option('-i, --indent <n>', 'number of spaces to indent with', str => parseInt(str), 2)
+	.option('-t, --tab', 'indent with tabs')
+	.parse(process.argv);
+
+const getInput = () => program.file ? readFile(program.file, 'utf-8') : getStdin();
+const writeOutput = output => program.out ? writeFile(program.out, output) : console.log(output);
+
+getInput().then(
+	input => writeOutput(
+		sqlFormatter.format(input, {
+			language: program.dialect,
+			indent: program.tab ? '\t' : ' '.repeat(program.indent)
+		})
+	)
+);
