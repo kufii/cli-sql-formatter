18a5e7707bbbce9b7bc98a6e314c23eb692d638e
Adrien Pyke <adpyke@gmail.com>
2019-09-08 00:23:31 -0400

    prettier, fix vulnerability

--- cli-sql-formatter.js
+++ cli-sql-formatter.js
@@ -17,17 +17,23 @@ program
 	.usage('[options]')
 	.option('-f, --file <file>', 'load SQL from a file')
 	.option('-o, --out <file>', 'output results to a file')
-	.option('-d, --dialect <dialect>', 'which dialect to format with (sql: Standard SQL, n1ql: Couchbase N1QL, db2: IBM DB2, pl/sql: Oracle PL/SQL)', /^(sql|n1ql|db2|pl\/sql)$/i, 'sql')
+	.option(
+		'-d, --dialect <dialect>',
+		'which dialect to format with (sql: Standard SQL, n1ql: Couchbase N1QL, db2: IBM DB2, pl/sql: Oracle PL/SQL)',
+		/^(sql|n1ql|db2|pl\/sql)$/i,
+		'sql'
+	)
 	.option('-i, --indent <n>', 'number of spaces to indent with', str => parseInt(str), 2)
 	.option('-t, --tab', 'indent with tabs')
 	.parse(process.argv);
 
-const getInput = () => program.file ? readFile(program.file, 'utf-8') : getStdin();
-const writeOutput = output => program.out ? writeFile(program.out, output) : console.log(output);
-const formatSql = sql => sqlFormatter.format(sql, {
-	language: program.dialect,
-	indent: program.tab ? '\t' : ' '.repeat(program.indent)
-});
+const getInput = () => (program.file ? readFile(program.file, 'utf-8') : getStdin());
+const writeOutput = output => (program.out ? writeFile(program.out, output) : console.log(output));
+const formatSql = sql =>
+	sqlFormatter.format(sql, {
+		language: program.dialect,
+		indent: program.tab ? '\t' : ' '.repeat(program.indent)
+	});
 const run = input => writeOutput(formatSql(input));
 
 getInput().then(run);
